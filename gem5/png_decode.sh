00c4a907ba27f09c863d55df73bb42a5c0e32b69
Felipe Graeff <Fxlipe115@users.noreply.github.com>
2019-05-06 03:44:22 -0300

    fixed typo

--- gem5/png_decode.sh
+++ gem5/png_decode.sh
@@ -3,5 +3,5 @@
 for input in $( ls ../algorithms/png_decode/images/*.png )
 do
 	build/ARM/gem5.opt emb/st_sim.py -c ../algorithms/png_decode/png_decode -o $input --cpu ARM_A7
-	cp ./m5out/stats.txt ..//results/png_decode/arm/$(basename ${input%.*}).txt
+	cp ./m5out/stats.txt ../results/png_decode/arm/$(basename ${input%.*}).txt
 done
