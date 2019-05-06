4d0347624d277b2da25c0bb970500c9a95a1eedb
Felipe Graeff <felipe.graeff@inf.ufrgs.br>
2019-05-06 03:41:44 -0300

    final results and graphs

--- results/png_decode/parse_results.sh
+++ results/png_decode/parse_results.sh
@@ -3,14 +3,14 @@
 frequency=900
 output=arm.csv
 
-echo "i.MX 6ULZ(Arm Cortex-A7)" > $output
-echo "${frequency}MHz" >> $output
+echo "# i.MX 6ULZ(Arm Cortex-A7)" > $output
+echo "# ${frequency}MHz" >> $output
 echo "entrada, ciclos, tempo(ms)" >> $output
 for file in $( ls ./arm/ )
 do
     while IFS=" " read -r property value remainder
     do
-        if [ "$property" = "host_inst_rate" ]
+        if [ "$property" = "system.cpu.numCycles" ]
         then
             time=$(awk -v val=$value -v freq=$frequency 'BEGIN { print val/freq }')
             echo "$(basename ${file%.*}), $value, $time" >> $output
